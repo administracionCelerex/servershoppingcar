7f86e33d04fec9ec105af1d188afd22a69cbc603
luissevilla <luissevilla111@gmail.com>
2022-08-17 17:37:58 -0500

    added new route and fixing model

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-
+require("dotenv").config();
 const mongoose = require("mongoose");
-const MONGOSERVER =
-  "celerexUser:j6XpDw6DN9ED26P@zohocrmcluster.izadd.mongodb.net";
-const MONGODBNAME = "celerex-dev?retryWrites=true&w=majority";
+const ProductModel = require("./models/products");
+
+const MONGOSERVER = process.env.MONGOSERVER;
+const MONGODBNAME = process.env.MONGODBNAME;
 const MONGOURI = `mongodb+srv://${MONGOSERVER}/${MONGODBNAME}`;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("/productos", (req, res) => {
+app.get("/products", (req, res) => {
   res.send("productos");
+});
+app.post("/products", (req, res) => {
+  res.send("insert new product");
 });
 
 try {
--- models/products.js
+++ models/products.js
@@ -1,5 +1,10 @@
-const shoppincarproducts = mongoose.model("shoppincarproducts", {
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productsSchema = new Schema({
   name: String,
   description: String,
-  price: String,
+  price: Number,
 });
+
+module.exports = mongoose.model("shoppincarproducts", productsSchema);
